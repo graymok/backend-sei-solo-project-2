b29a5c083a514c981a95b6a59a2570718afc96d9
Tim Mok <timothyjmok@gmail.com>
2021-04-09 17:51:27 -0400

    updated users routes and controllers

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -3,7 +3,28 @@ const models = require('../models')
 
 const usersControllers = {}
 
+usersControllers.createUser = async (req, res) => {
+    try {
+        const country = await models.country.findOne({
+            where: {
+                country: req.body.country
+            }
+        })
+        
+        const user = await models.user.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
+        })
 
+        const countryChoice = await user.setCountry(country)
+        res.json({ message: 'User created' })
+    } catch (error) {
+        res.status(400)
+        res.json({ error })
+        
+    }
+}
 
 
 
--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const usersRoutes = express.Router()
 const usersControllers = require('../controllers/usersControllers')
 
-
+usersControllers.post('/users', usersControllers.createUser)
 
 
 
