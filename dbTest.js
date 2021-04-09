6690d2d985ad78411c466c3fcb33a2c87e68c92a
Tim Mok <timothyjmok@gmail.com>
2021-04-09 17:10:59 -0400

    dbtest complete, all associations work as intended

--- dbTest.js
+++ dbTest.js
@@ -1,8 +1,6 @@
 const models = require('./models')
 
-
-
-const testing = async () => {
+/* const testing = async () => {
     const createUser1 = await models.user.create({
         name: 'Test1',
         email: 'test1@test.com',
@@ -13,6 +11,16 @@ const testing = async () => {
         name: 'Test2',
         email: 'test2@test.com',
         password: '1234'
+    })
+
+    const testArticle = await models.article.create({
+        title: 'Today in breaking news, a Walrus',
+        url: 'url',
+        image: 'image url'
+    })
+
+    const article = await models.article.findOne({
+        id: '1'
     })
 
     const foundUser1 = await models.user.findOne({
@@ -39,8 +47,13 @@ const testing = async () => {
         }
     })
 
-    const koreaUser1 = await foundUser1.addCountry(southKorea)
-    const koreaUser2 = await foundUser2.addCountry(southKorea)
+    // testing country having multiple users association
+    const koreaUser1 = await foundUser1.setCountry(southKorea)
+    const koreaUser2 = await foundUser2.setCountry(southKorea)
+
+    // testing users having multiple articles and articles having multiple users
+    const articleUser1 = await foundUser1.addArticle(article)
+    const testArticle2 = await article.addUser(foundUser2)
 }
 
-testing()
+testing() */
