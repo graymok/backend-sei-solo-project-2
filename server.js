dadf2d8da6f7367177c895b683354b69df7fa4d6
Tim Mok <timothyjmok@gmail.com>
2021-04-12 15:46:59 -0400

    prepare for heroku deployment

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use('/news', newsRoutes)
 app.use('/users', usersRoutes)
 
 
-const PORT = process.env.port || 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`We're live on ${PORT}`)
   routesReport.print()
