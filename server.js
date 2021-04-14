dac567fb17e7489eb2c9320709533fa60248ea7d
Tim Mok <timothyjmok@gmail.com>
2021-04-14 12:00:19 -0400

    added CORS snippet for troubleshooting

--- server.js
+++ server.js
@@ -14,6 +14,12 @@ app.use(express.json())
 app.use(require('cors')())
 app.use(morgan('tiny'))
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use('/news', newsRoutes)
 app.use('/users', usersRoutes)
 
